95a836326e7104938371bb6eaaf6d94b533e4e71
capJavert <ante.baric3@gmail.com>
2015-11-20 14:01:14 +0100

    less integration

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -2,6 +2,15 @@
 
 $config = [
     'components' => [
+	    'assetManager' => [
+            'converter' => [
+                'class' => 'cakebake\lessphp\AssetConverter',
+                'compress' => true, // Optional: You can tell less.php to remove comments and whitespace to generate minimized css files.
+                'useCache' => true, // Optional: less.php will save serialized parser data for each .less file. Faster, but more memory-intense.
+                //'cacheDir' => null, // Optional: is passed to the SetCacheDir() method. By default "cakebake\lessphp\runtime" is used.
+                //'cacheSuffix' => true, // Optional: Filename suffix to avoid the browser cache and force recompiling by configuration changes
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '1XlvWM0lciC5SDX9Fonx53hFq9pJUqyl',
